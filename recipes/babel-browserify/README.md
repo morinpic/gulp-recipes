bfb69d586825454d9904cb3255a820d3d361d836
kenta-mori <kenta_mori@waku-2.com>
2015-03-24 18:23:43 +0900

    - add babel env

---
+++ recipes/babel-browserify/README.md
@@ -0,0 +1,18 @@
+Babel + browserify
+=======
+Simple [Babel](https://github.com/babel/babel) Development Environment.
+
+Using [babelify](https://github.com/babel/babelify) of [browserify](https://github.com/substack/node-browserify) transform.
+
+Installation
+------------
+```
+npm start
+```
+
+Development
+----
+```
+npm run server
+```
+Automatically generate `.tmp/js/bundle.js` when you touch `assets/js/**/*.js`
---
+++ recipes/babel-browserify/gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var babelify = require('babelify');
+var rimraf = require('rimraf');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// clean:tmp
+gulp.task('clean:tmp', function(cb) {
+  rimraf('.tmp', cb);
+});
+
+// browserify
+gulp.task('browserify', function() {
+  var b = browserify({
+    entries: ['./assets/js/app.js'],
+    transform: [babelify]
+  });
+  return b.bundle()
+    .on('error', $.notify.onError('<%= error.message %>'))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('.tmp/js'))
+    .pipe(reload({stream:true}));
+});
+
+// copy:html
+gulp.task('copy:html', function() {
+  return gulp.src('assets/**/*.html')
+    .pipe(gulp.dest('.tmp'))
+    .pipe(reload({stream:true}));
+});
+
+// copy:css
+gulp.task('copy:css', function() {
+  return gulp.src('assets/css/**/*.css')
+    .pipe(gulp.dest('.tmp/css'))
+    .pipe(reload({stream:true}));
+});
+
+// browser-sync
+gulp.task('browser-sync', function() {
+  browserSync({server: {baseDir: '.tmp'}});
+});
+
+// watch
+gulp.task('watch', ['browser-sync'], function() {
+  gulp.watch('assets/**/*.html', ['copy:html']);
+  gulp.watch('assets/js/**/*.js', ['browserify']);
+  gulp.watch('assets/css/**/*.css', ['copy:css']);
+});
+
+// server
+gulp.task('server', function() {
+  runSequence(
+    'clean:tmp',
+    ['browserify', 'copy:html', 'copy:css'],
+    'watch'
+  );
+});
